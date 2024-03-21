d3702d299e4439bafd05775be87500ae0bf50c5a
tariq.boutaguia.etu <tariq.boutaguia.etu@univ-lille.fr>
2024-03-21 16:55:21 +0100

    Itération 5 juste pour le controlleur d'agenda, il reste le controlleur des évenements.

--- agenda/src/main/java/TP2/agenda/AgendaControlleur.java
+++ agenda/src/main/java/TP2/agenda/AgendaControlleur.java
@@ -21,40 +21,61 @@ public class AgendaControlleur {
     private AgendaServiceItf service;
 	@Autowired
 	private PersonneServiceItf serviceRegister;
-
+    
 	@GetMapping("/liste")
-	public String home(Model model) {
-        Iterable<Agenda> agendas = service.getAllAgendas();
-        model.addAttribute("agendas", agendas);
-        return "liste";
+	public String home(Model model, HttpSession session) {
+		if(session.getAttribute("personne") == null) {
+			return "redirect:/home";
+		} else {
+            Iterable<Agenda> agendas = service.getAllAgendas();
+            model.addAttribute("agendas", agendas);
+            return "liste";
+		}
     }
 	
 	@PostMapping("/createAgenda")
-    public String addAgenda(@RequestParam String nom) {
-        service.ajoutAgenda(nom);
-        return "redirect:/liste";
+    public String addAgenda(@RequestParam String nom, HttpSession session) {
+		if(session.getAttribute("personne") == null) {
+			return "redirect:/home";
+		} else {
+            service.ajoutAgenda(nom);
+            return "redirect:/liste";
+		}
     }
 	
 	@GetMapping("/mesAgendas/liste")
 	public String MesAgenda(Model model, HttpServletRequest request) {
 		HttpSession session = request.getSession();
-	    Personne personne = (Personne) session.getAttribute("personne");
-        Iterable<Agenda> agendas = service.getAgendasByUser(personne);
-        model.addAttribute("agendas", agendas);
-		return "mesAgendas";
+		if(session.getAttribute("personne") == null) {
+			return "redirect:/home";
+		} else {
+			 Personne personne = (Personne) session.getAttribute("personne");
+             Iterable<Agenda> agendas = service.getAgendasByUser(personne);
+             model.addAttribute("agendas", agendas);
+		     return "mesAgendas";
+		}
+	   
     }
 	
 	@GetMapping("/mesAgendas/new")
-	public String MesAgendaNouveau() {
-		return "agenda";
+	public String MesAgendaNouveau(HttpSession session) {
+		if(session.getAttribute("personne") == null) {
+			return "redirect:/home";
+		} else {
+		    return "agenda";
+		}
     }
 	
 	@PostMapping("/agenda/new")
 	public String addAgendaUser(@RequestParam String nom, HttpServletRequest request) {
 		HttpSession session = request.getSession();
-	    String email = (String) session.getAttribute("email");
-	    Personne personne = serviceRegister.getPersonneByEmail(email);
-        service.ajoutAgenda(nom, personne);
-        return "redirect:/mesAgendas/liste";
+		if(session.getAttribute("personne") == null) {
+			return "redirect:/home";
+		} else {
+			String email = (String) session.getAttribute("email");
+	        Personne personne = serviceRegister.getPersonneByEmail(email);
+            service.ajoutAgenda(nom, personne);
+            return "redirect:/mesAgendas/liste";
+		} 
     }
 }
